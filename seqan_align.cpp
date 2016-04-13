34251410125bcbcdbcd2dbf07a527d4b818067a5
Ryan Wick <rrwick@gmail.com>
2016-04-13 11:22:31 +1000

    Working on Seqan alignment

--- seqan_align.cpp
+++ seqan_align.cpp
@@ -18,14 +18,18 @@ using namespace std::chrono;
 extern "C" {
 
 typedef Dna5String TSequence;
-typedef Align<Dna5String, ArrayGaps> TAlign;
+typedef Align<TSequence, ArrayGaps> TAlign;
 typedef Row<TAlign>::Type TRow;
 typedef Seed<Simple> TSeed;
 typedef SeedSet<TSeed> TSeedSet;
+typedef Row<TAlign>::Type TRow;
+typedef Iterator<TRow>::Type TRowIterator;
 
 typedef std::tuple<std::string, int, int> Kmer;
 typedef std::map<std::string, std::tuple<int, int>> KmerDict;
 typedef std::tuple<int, int, int, int> CommonLocation;
+
+enum CigarType {MATCH, INSERTION, DELETION};
 
 std::vector<Kmer> getSeqKmers(std::string seq, int strLen, int kSize)
 {
@@ -72,123 +76,83 @@ std::vector<CommonLocation> getCommonLocations(std::vector<Kmer> s1Kmers, std::v
 	return commonLocations;
 }
 
-struct LocationCompare
+
+void semiGlobalAlign(char * s1, char * s2, int s1Len, int s2Len, int kSize, int bandSize)
 {
-    bool operator()(CommonLocation const &l1, CommonLocation const &l2)
-    {
-    	if (std::get<0>(l1) < std::get<0>(l2))
-    		return true;
-	    if (std::get<0>(l1) > std::get<0>(l2))
-	    	return false;
-	    return std::get<2>(l1) < std::get<2>(l2);
-    }
-};
-
-std::vector<CommonLocation> mergeAdjacentLocations(std::vector<CommonLocation> locations)
-{
-	std::sort(std::begin(locations), std::end(locations), LocationCompare());
-
-	std::vector<CommonLocation> mergedLocations;
-	CommonLocation currentLocation = locations[0];
-	int currentS1Start = std::get<0>(currentLocation);
-	for (int i = 1; i < locations.size(); ++i)
-	{
-		CommonLocation location = locations[i];
-		if (std::get<0>(location) == currentS1Start + 1)
-		{
-			std::get<1>(currentLocation) += 1;
-			std::get<3>(currentLocation) += 1;
-		}
-		else
-		{
-			mergedLocations.push_back(currentLocation);
-			currentLocation = location;
-			currentS1Start = std::get<0>(location);
-		}
-	}
-	mergedLocations.push_back(currentLocation);
-
-	return mergedLocations;
-}
-
-
-void semiGlobalAlign(char * s1, char * s2, int s1Len, int s2Len)
-{
-	int kSize = 7;
+	long long time1 = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
 
 	std::string s1Str(s1);
 	std::string s2Str(s2);
-
 	std::vector<Kmer> s1Kmers = getSeqKmers(s1Str, s1Len, kSize);
 	std::vector<Kmer> s2Kmers = getSeqKmers(s2Str, s2Len, kSize);
 	std::vector<CommonLocation> commonLocations = getCommonLocations(s1Kmers, s2Kmers);
-	std::vector<CommonLocation> mergedLocations = mergeAdjacentLocations(commonLocations);
 
-	for (int i = 0; i < mergedLocations.size(); ++i)
+    long long time2 = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
+
+	TSeedSet seedSet;
+	for (int i = 0; i < commonLocations.size(); ++i)
 	{
-		CommonLocation l = mergedLocations[i];
-		std::cout << std::get<0>(l) << " " << std::get<1>(l) << " " << std::get<2>(l) << " " << std::get<3>(l) << std::endl;
+		CommonLocation l = commonLocations[i];
+		TSeed seed(std::get<0>(l), std::get<2>(l), std::get<1>(l), std::get<3>(l));
+    	if (!addSeed(seedSet, seed, 1, Merge()))
+        	addSeed(seedSet, seed, Single());
 	}
 
-
-
-
-    long long ms_before = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
+    long long time3 = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
 
     TSequence sequenceH = s1;
     TSequence sequenceV = s2;
 
-    Score<int, Simple> scoringScheme(1, -1, -1);
-    AlignConfig<true, true, true, true> alignConfig;
-
-	TSeedSet seedSet;
-	for (int i = 0; i < mergedLocations.size(); ++i)
-	{
-		CommonLocation l = mergedLocations[i];
-		addSeed(seedSet, TSeed(std::get<0>(l), std::get<2>(l), std::get<1>(l), std::get<3>(l)), Single());
-	}
-
-
 	String<TSeed> seedChain;
 	chainSeedsGlobally(seedChain, seedSet, SparseChaining());
+
+    long long time4 = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
 
 	Align<Dna5String, ArrayGaps> alignment;
     resize(rows(alignment), 2);
     assignSource(row(alignment, 0), sequenceH);
     assignSource(row(alignment, 1), sequenceV);
+    Score<int, Simple> scoringScheme(1, -1, -1);
+    AlignConfig<true, true, true, true> alignConfig;
+    int result = bandedChainAlignment(alignment, seedChain, scoringScheme, alignConfig, bandSize);
 
-    int result = bandedChainAlignment(alignment, seedChain, scoringScheme, alignConfig, 20);
+    long long time5 = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
 
-    std::cout << "Score: " << result << std::endl;
-    std::cout << alignment << std::endl;
+    // std::cout << alignment << std::endl;
+
+	int alignmentLength = std::max(length(row(alignment, 0)), length(row(alignment, 1)));
+
+	for (int i = 0; i < alignmentLength; ++i)
+	{
+		std::cout << row(alignment, 0)[i] << row(alignment, 1)[i] << std::endl;
+
+	}
+
+	// for (int i = 0; i < length(rows(alignment)); ++i)
+ //    {
+ //    	TRowIterator it = iter(row(alignment, i), 0);
+ //        TRowIterator itEnd = iter(row(alignment, i), aliLength);
+ //        int pos = 0;
+ //        std::cout << "Row " << i << " contains gaps at positions: ";
+ //        std::cout << std::endl;
+ //        while (it != itEnd)
+ //        {
+ //            if (isGap(it))
+ //                std::cout << pos << std::endl;
+ //            ++it;
+ //            ++pos;
+ //        }
+ //    }
 
 
+    std::cout << "Score: " << result << std::endl << std::endl;
 
-
-
-
-    // TSeed seed(1681, 1073, 1691, 1083);
-
-
-    // extendSeed(seed, seq1, seq2, EXTEND_BOTH, scoringScheme, 3, GappedXDrop());
-
-    // std::cout << "seed1: " << infix(seq1, beginPositionH(seed), endPositionH(seed)) << "\n"
-    //           << "seed2: " << infix(seq2, beginPositionV(seed), endPositionV(seed)) << "\n";
-
-
-    // TAlign align;
-    // resize(rows(align), 2);
-    // assignSource(row(align, 0), infix(seq1, beginPositionH(seed),
-    //                                   endPositionH(seed)));
-    // assignSource(row(align, 1), infix(seq2, beginPositionV(seed),
-    //                                   endPositionV(seed)));
-
-    // globalAlignment(align, scoringScheme, alignConfig);
-    // std::cout << align << "\n";
-
-    long long ms_after = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
-    long long elapsedTime = ms_after - ms_before;
-    std::cout << "Milliseconds: " << elapsedTime << std::endl;
+    std::cout << "Milliseconds to find common kmers: " << time2 - time1 << std::endl;
+    std::cout << "Milliseconds to find add seeds:    " << time3 - time2 << std::endl;
+    std::cout << "Milliseconds to find chain seeds:  " << time4 - time3 << std::endl;
+    std::cout << "Milliseconds to perform alignment: " << time5 - time4 << std::endl;
+    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "Total milliseconds:                " << time5 - time1 << std::endl;
 }
 
 
